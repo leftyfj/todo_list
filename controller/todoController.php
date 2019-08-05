3aa9f2af7793079bd37c50f4a28e38864bab6acc
leftyfj <gwall59@gmail.com>
2019-08-05 20:59:10 +0900

    todoController.phpを修正したが、タスクがDBに登録されない。

--- controller/todoController.php
+++ controller/todoController.php
@@ -9,23 +9,34 @@ class todoController {
     // public $project;
     // public $genre;
     // public $tag;
-    function create($title,$memo,$priority,$start,$deadline) {
+    function create() {
+      $type = $_POST['type'];
+      $title = $_POST['title'];
+      $memo = $_POST['memo'];
+      $priority = $_POST['priority'];
+      $start = $_POST['start'];
+      $deadline = $_POST['deadline'];
+      // $project = $_POST['project'];
+      // $genre = $_POST['genre'];
+      // $tage = $_POST['tag'];
+
       //パラメータ取得
-      $this->title = $title;
-      $this->memo = $memo;
-      $this->priority = $priority;
-      $this->start = $start;
-      $this->deadline = $deadline;
+      // $this->title = $title;
+      // $this->memo = $memo;
+      // $this->priority = $priority;
+      // $this->start = $start;
+      // $this->deadline = $deadline;
 
       //取得した値を配列に格納
       $data = array(
-              'title' => $this->title,
-              "memo" => $this->memo,
-              "priority" => $this->priority,
-              "start" => $this->start,
-              "deadline" => $this->deadline
+              "title" => $title,
+              "memo" => $memo,
+              "priority" => $priority,
+              "start" => $start,
+              "deadline" => $deadline,
           );
 
+      var_dump($data);
       //DB保存
       $todo = new Todo;
       $todo -> regist($data);
--- views/todo/complete.php
+++ views/todo/complete.php
@@ -4,19 +4,19 @@ require_once './../../config/config.php';
 require_once './../../model/todo.php';
 require_once './../../controller/todoController.php';
 
-$type = $_POST['type'];
-$title = $_POST['title'];
-$memo = $_POST['memo'];
-$priority = $_POST['priority'];
-$start = $_POST['start'];
-$deadline = $_POST['deadline'];
+// $type = $_POST['type'];
+// $title = $_POST['title'];
+// $memo = $_POST['memo'];
+// $priority = $_POST['priority'];
+// $start = $_POST['start'];
+// $deadline = $_POST['deadline'];
 // $project = $_POST['project'];
 // $genre = $_POST['genre'];
 // $tage = $_POST['tag'];
 
 if(isset($type) && $type = 'new'){
   $class = new todoController;
-  $class -> create($title,$memo,$priority,$start,$deadline);
+  $class -> create();
 }
 
 
